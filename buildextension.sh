473283f1a6e561743f97cee51b060fbe38e8634b
Marcus Gutierrez <marcusgttrz@gmail.com>
2020-01-20 23:15:28 -0600

    Fix for options file

--- buildextension.sh
+++ buildextension.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 browser=$1
+version=$2
+
+if [ -z $version ]; then
+    version=$(date +%s)
+fi
 
 cwd=$(pwd)
-newArtifactFolder="$cwd/ext-artifacts/$browser/ext-$(date +%s)/"
+newArtifactFolder="$cwd/ext-artifacts/$browser/ext-$version/"
+
+curFiles=($newArtifactFolder/*)
+if [ ${#curFiles[@]} -gt 1 ]; then 
+    echo "Current version is already built. Replacing.."; 
+    rm -r $newArtifactFolder 
+fi
+
 mkdir -pv $newArtifactFolder
 cp $cwd/$browser/* $newArtifactFolder
 cp $cwd/shared/* $newArtifactFolder
