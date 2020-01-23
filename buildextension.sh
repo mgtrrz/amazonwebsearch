c46652f750ac4fafb6bd9a375ec32100316fe650
Marcus Gutierrez <marcusgttrz@gmail.com>
2020-01-22 21:17:21 -0600

    Prepares master for Chrome release

--- buildextension.sh
+++ buildextension.sh
@@ -1,22 +1,46 @@
 #!/bin/bash
 
-browser=$1
-version=$2
+app_name="amazon_web_search"
+
+option=$1
+browser=$2
+version=$3
 
 if [ -z $version ]; then
     version=$(date +%s)
 fi
 
-cwd=$(pwd)
-newArtifactFolder="$cwd/ext-artifacts/$browser/ext-$version/"
+function pkg_ext() {
+    cwd=$(pwd)
+    releasesFolder="$cwd/ext-artifacts/releases/"
+    newArtifactFolder="$cwd/ext-artifacts/$browser/ext-$version/"
 
-curFiles=($newArtifactFolder/*)
-if [ ${#curFiles[@]} -gt 1 ]; then 
-    echo "Current version is already built. Replacing.."; 
-    rm -r $newArtifactFolder 
+    mkdir -pv $releasesFolder
+    zip -r "$releasesFolder/${browser}_${app_name}_${version}.zip" ${newArtifactFolder}*
+}
+
+function build_ext() {
+    cwd=$(pwd)
+    newArtifactFolder="$cwd/ext-artifacts/$browser/ext-$version/"
+
+    curFiles=($newArtifactFolder/*)
+    if [ ${#curFiles[@]} -gt 1 ]; then 
+        echo "Current version is already built. Replacing.."; 
+        rm -r $newArtifactFolder 
+    fi
+
+    mkdir -pv $newArtifactFolder
+    cp $cwd/$browser/* $newArtifactFolder
+    cp $cwd/shared/* $newArtifactFolder
+    cp -R $cwd/icons $newArtifactFolder
+}
+
+if [[ $option == "package" ]]; then
+    build_ext;
+    pkg_ext;
+elif [[ $option == "build" ]]; then
+    build_ext;
+else
+    echo "Please enter a valid option: build, package"
+    exit 1
 fi
-
-mkdir -pv $newArtifactFolder
-cp $cwd/$browser/* $newArtifactFolder
-cp $cwd/shared/* $newArtifactFolder
-cp -R $cwd/icons $newArtifactFolder
